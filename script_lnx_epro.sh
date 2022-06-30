7addd8fab35e1dc815dbb47c6b7d52cfd7f19706
rodrigogreff <rodrigo_greff@hotmail.com>
2022-06-29 22:54:36 -0300

    instalar as ferramentas de linha de comando SQL Server

--- script_lnx_epro.sh
+++ script_lnx_epro.sh
@@ -29,6 +29,27 @@ sudo /opt/mssql/bin/mssql-conf setup
 # verifique se o serviço está em execução
 systemctl status mssql-server --no-pager
 
+# Instalar as ferramentas de linha de comando SQL Server
+
+# Etapas a seguir para instalar o mssql-tools no Ubuntu e o curl 
+apt-get install curl
+
+# Importe as chaves GPG do repositório público.
+curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
+
+# Registre o repositório do Ubuntu
+curl https://packages.microsoft.com/config/ubuntu/20.04/prod.list | sudo tee /etc/apt/sources.list.d/msprod.list
+
+# Atualize a lista de fontes e execute o comando de instalação com o pacote do desenvolvedor do unixODBC
+apt-get install mssql-tools unixodbc-dev
+
+# Atualizar para a versão mais recente do mssql-tools
+sudo apt-get update 
+sudo apt-get install mssql-tools
+
+# Adicione /opt/mssql-tools/bin/ à variável PATH de ambiente para tornar o sqlcmd ou bcp acessível no shell bash
+echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.bash_profile
+
 # Criação de diretório
 mkdir /home/adminetab/epro
 mkdir /home/adminetab/sn
